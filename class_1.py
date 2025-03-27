8f00f49acf32f3ddea29bc3b453a91429e4b792e
Seanmcg-97 <seanmcg12@live.co.uk>
2025-03-27 02:09:05 +0000

    Add files via upload

--- class_1.py
+++ class_1.py
@@ -8,7 +8,7 @@ MIT License
 
 Copyright (c) 2025 Sean McGeoghegan
 
-See LICENSE.txt for details
+See LICEnSE.txt for details
 
 """
 
@@ -22,14 +22,14 @@ rank = comm.Get_rank()
 nworkers = comm.Get_size()
 
 
-class Monte_Carlo:
+class MonteCarlo:
     """
     This class is for running Monte Carlo simulations to find the integral, 
     variance and error for the area of a circle.
 
     """
-   
-    def __init__(self, start, end, N, f, *varis):
+
+    def __init__(self, start, end, num, f, *varis):
         """
         Class initialisation for Monte Carlo
     
@@ -37,70 +37,71 @@ class Monte_Carlo:
         start = Starting coordinate in the square
         end = Final coordinate in the square
         f = The function being passed through
-        vars = Any variables required for the function in use
+        varis = Any variables required for the function in use
         values = Class initialisation for f-string print result
 
         """
         self.start = start   # Starting point of integral
         self.end = end   # End point of integral
         self.f = f   # Function to be passed through MC
-        self.N = N   # Number of iterations to be performed by MC
+        self.num = num   # number of iterations to be performed by MC
         self.vars = varis   # Variable(s) within an array for a function
         self.values = 0     # Used for final results
 
-    
     def __str__(self):
         """
         F-string for final results
 
         """
-        return f"(Integral: {self.values[0]}, Var: {self.values[1]}, Err: {self.values[2]})"
+        return f"(integral: {self.values[0]}, Var: {self.values[1]}, Err: {self.values[2]})"
 
-    
-    def Integral(self):
+
+    def integral(self):
         """
         This function is used to calculate the integral of a function, via random walks.
 
         """
-        
+
         # Setting the dimension in use as the length of start
-        D = len(self.start)   
+        d = len(self.start)
 
         # Set-up for Random walks between rank 0 and nworkers, using random seed generator
-        ss = SeedSequence(23456)  
-        nworkers_seed = ss.spawn(nworkers)  
-        
+        ss = SeedSequence(23456)
+        nworkers_seed = ss.spawn(nworkers)
+
         # Ensuring each rank performs random walks
-        Random_gen = [default_rng(s) for s in nworkers_seed]
-        R_num = Random_gen[rank].random((self.N, D))
+        random_gen = [default_rng(s) for s in nworkers_seed]
+        r_num = random_gen[rank].random((self.num, d))
 
         # Setting up initial array's
-        Sum_func = np.zeros_like(self.f(np.zeros(D), *self.vars), dtype=np.float64)
-        Sum_func_sq = np.zeros_like(self.f(np.zeros(D), *self.vars), dtype=np.float64)
-        Final_func = np.array(0, dtype=np.float64)
-        Final_func_sq = np.array(0, dtype=np.float64)
+        s_func = np.zeros_like(self.f(np.zeros(d), *self.vars), dtype=np.float64)
+        s_func_sq = np.zeros_like(self.f(np.zeros(d), *self.vars), dtype=np.float64)
+        final_func = np.array(0, dtype=np.float64)
+        final_func_sq = np.array(0, dtype=np.float64)
 
-
-        for n in R_num:
-            for i in range(D):
+        # double for loop to approximately calculate the integral over a select domain
+        for n in r_num:
+            for i in range(d):
                 n[i] = n[i] * (self.end[i] - self.start[i]) + self.start[i]
-            Sum_func += (self.f(n, *self.vars))
-            Sum_func_sq += (self.f(n, *self.vars))**2
+            s_func += (self.f(n, *self.vars))
+            s_func_sq += (self.f(n, *self.vars))**2
 
-        comm.Allreduce(Sum_func, Final_func)
-        comm.Allreduce(Sum_func_sq, Final_func_sq)
+        # Setting up a reduction of for loop values before broadcasting to workers
+        comm.Allreduce(s_func, final_func)
+        comm.Allreduce(s_func_sq, final_func_sq)
 
-        # Used to set up numerator/denominator of (a*d - c * b)/N
+        # Used to set up numerator/denominator of (a*d - c * b)/n
         ad_cb = np.prod(np.array(self.end) - np.array(self.start))
-        inv_N = 1 / (self.N * nworkers)
+        inv_n = 1 / (self.num * nworkers)
 
-        Integral = ad_cb * inv_N * Final_func  
-        Variance = inv_N * (Final_func_sq * inv_N - (Final_func * inv_N) ** 2)  
-        Error = ad_cb * np.sqrt(Variance)  
-        self.values = np.array([Integral, Variance, Error])
+        # Variable set-ups for calculations for the integral, variance and error values
+        integral = ad_cb * inv_n * final_func
+        variance = inv_n * (final_func_sq * inv_n - (final_func * inv_n) ** 2)  
+        error = ad_cb * np.sqrt(variance)
+        self.values = np.array([round(integral, 5), round(variance, 10), round(error, 5)])
 
         return self.values
-    
+
     def infinity(self):
         '''
         This function is used for infinite or improper cases
@@ -108,49 +109,47 @@ class Monte_Carlo:
         '''
 
         # Setting the dimension in use as the length of start
-        D = len(self.start)
+        d = len(self.start)
 
         # Set-up for Random walks between rank 0 and nworkers, using random seed generator
-        ss = SeedSequence(23456)  
-        nworkers_seed = ss.spawn(nworkers)  
+        ss = SeedSequence(23456)
+        nworkers_seed = ss.spawn(nworkers)
 
         # Ensuring each rank performs random walks
-        Random_gen = [default_rng(s) for s in nworkers_seed]
-        R_num = Random_gen[rank].random((self.N, D))
+        random_gen = [default_rng(s) for s in nworkers_seed]
+        r_num = random_gen[rank].random((self.num, d))
 
         # Setting up initial array's
-        Sum_func = np.zeros_like(self.f(np.zeros(D), *self.vars), dtype=np.float64)
-        Sum_func_sq = np.zeros_like(self.f(np.zeros(D), *self.vars), dtype=np.float64)
+        s_func = np.zeros_like(self.f(np.zeros(d), *self.vars), dtype=np.float64)
+        s_func_sq = np.zeros_like(self.f(np.zeros(d), *self.vars), dtype=np.float64)
+        final_func = np.empty(d, dtype=np.float64)
+        final_func_sq = np.empty(d, dtype=np.float64)
 
-        Final_func = np.empty(D, dtype=np.float64)
-        Final_func_sq = np.empty(D, dtype=np.float64)
+        # a_inf and b_inf are used as the variable names for the bottom 
+        # and top values of the integral in infinite/improper cases.
+        a_inf, b_inf = -1, 1
 
-        # a_inf and b_inf are used as the variable names for the bottom and top values of the integral 
-        # in infinite/improper cases.
-        a_inf, b_inf = -1, 1  
-
-        
-        for n in R_num:
-            for i in range(D):
+        # double for loop to approximately calculate the integral over a select domain. Including
+        # the added factors within the integral calculation
+        for n in r_num:
+            for i in range(d):
                 n[i] = n[i] * (b_inf - a_inf) + a_inf
-            x = n/(1-n**2) 
-            y = (1+n**2)/((1-n**2)**2) 
-            Sum_func += self.f(x, *self.vars) * y
-            Sum_func_sq += (self.f(x, *self.vars) * y)**2
-        
+            x = n/(1-n**2)
+            y = (1+n**2)/((1-n**2)**2)
+            s_func += self.f(x, *self.vars) * y
+            s_func_sq += (self.f(x, *self.vars) * y)**2
 
-        comm.Allreduce(Sum_func, Final_func)
-        comm.Allreduce(Sum_func_sq, Final_func_sq)
+        # Setting up a reduction of for loop values before broadcasting to workers
+        comm.Allreduce(s_func, final_func)
+        comm.Allreduce(s_func_sq, final_func_sq)
 
-        # Used to set up numerator/denominator of (a*d - c * b)/N
+        # Used to set up numerator/denominator of (a*d - c * b)/n
         ad_cb = np.prod(b_inf - a_inf)
-        inv_N = 1 / (self.N * nworkers)
+        inv_n = 1 / (self.num * nworkers)
 
         # Calculation for the integral, variance, and error
-        Integral = np.mean(ad_cb * inv_N * Final_func)
-        Variance = inv_N * np.mean((Final_func_sq * inv_N - (Final_func * inv_N) ** 2)) ### FIX THIS 
-        Error = ad_cb * np.sqrt(Variance)
-
-        self.values = np.array([Integral, Variance, Error])
+        integral = np.mean(ad_cb * inv_n * final_func)
+        variance = inv_n * np.mean((final_func_sq * inv_n - (final_func * inv_n) ** 2))
+        error = ad_cb * np.sqrt(variance)
+        self.values = np.array([round(integral, 5), round(variance, 9), round(error, 5)])
         return self.values
-
--- task.py
+++ task.py
@@ -1,34 +1,27 @@
 #!/bin/python3
+"""
+Module containing set-up for calculations of given tasks.
+
+MIT License
+
+Copyright (c) 2025 Sean McGeoghegan
+
+See LICENSE.txt for details
+
+"""
 
 import time as t
+import matplotlib.pyplot as plt
 import numpy as np
 from mpi4py import MPI
-from class_1 import Monte_Carlo
-
+from class_1 import MonteCarlo
 
 # MPI.Init()
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nworkers = comm.Get_size()
 
-###--------------------### Task 1 ####----------------------###
-
-def shape(x, R):
-    """
-    This function is used for any circular-shaped object. 
-    Passed through if/else statement to check if some point in space
-    is within a circular object, or lies upon its line/surface
- 
-    Parameters: 
-        x: Describes a random point in space
-        R: Describes the radius of a circle
-
-    """
-
-    if round(np.sum(np.square(x)), 2) <= (R**2):
-        return True
-    else:
-        return False
+###--------------------### Test of Monte Carlo sim ####----------------------###
 
 def quad(x, a, b):
     """
@@ -39,35 +32,50 @@ def quad(x, a, b):
         x: Variable
         b: Constant term
 
-    """    
+    """
     return a*x**2 + b
 
-
-pi = np.pi
-
+# Set-up of variables to test MC with quadrature function "quad"
 N = int(100000)
 x2 = np.array([2])
 y2 = np.array([4])
 v = np.array([1, 2])
 
-
-Test = Monte_Carlo(x2, y2, N, quad, *v)
-I = Monte_Carlo.Integral(Test)
+Test = MonteCarlo(x2, y2, N, quad, *v)
+I = MonteCarlo.integral(Test)
 
 if rank == 0:
     print(f"The integral of x^2 between {x2} and {y2}: {Test}")
 
+###--------------------### Task 1 ####----------------------###
 
-# Variable set-up for spheres of dimensions: 2-5
+def shape(x, r):
+    """
+    This function is used for any circular-shaped object. 
+    Passed through if/else statement to check if some point in space
+    is within a circular object, or lies upon its line/surface
+ 
+    Parameters: 
+        x: Describes a random point in space
+        r: Describes the radius of a circle
+
+    """
+
+    if round(np.sum(np.square(x)), 2) <= (r**2):
+        return True
+    else:
+        return False
+
+# Variable set-up for spheres of dimensions (D): 2-5
 D_2 = 2
 D_3 = 3
 D_4 = 4
 D_5 = 5
 
-# Variable set-up for the radius used for each dimension
+# Variable set-up for the radius (R) used for each dimension
 R1 = np.array([1])
 
-# Variable set-up for the start and end points being repeated across an array dependent on dimension size 
+# Variable set-up for the start (S) and end (E) points being repeated across an array dependent on dimension size
 S1 = np.repeat(-R1, D_2)
 E1 = np.repeat(R1, D_2)
 
@@ -82,18 +90,20 @@ E4 = np.repeat(R1, D_5)
 
 t1_a = t.time()
 
-# Monte Carlo simulation for a circle, sphere, 4D & 5D hypersphere 
-circle = Monte_Carlo(S1, E1, N, shape, *R1)
-I_c = Monte_Carlo.Integral(circle)
+# Monte Carlo simulation for a circle, sphere, 4D & 5D hypersphere
+circle = MonteCarlo(S1, E1, N, shape, *R1)
+I_c = MonteCarlo.integral(circle)
 
-sphere = Monte_Carlo(S2, E2, N, shape, *R1)
-I_s = Monte_Carlo.Integral(sphere)
+sphere = MonteCarlo(S2, E2, N, shape, *R1)
+I_s = MonteCarlo.integral(sphere)
 
-hypersphere_4D = Monte_Carlo(S3, E3, N, shape, *R1)
-I_hs_4 = Monte_Carlo.Integral(hypersphere_4D)
+hypersphere_4D = MonteCarlo(S3, E3, N, shape, *R1)
+I_hs_4 = MonteCarlo.integral(hypersphere_4D)
 
-hypersphere_5D = Monte_Carlo(S4, E4, N, shape, *R1)
-I_hs_5 = Monte_Carlo.Integral(hypersphere_5D)
+hypersphere_5D = MonteCarlo(S4, E4, N, shape, *R1)
+I_hs_5 = MonteCarlo.integral(hypersphere_5D)
+
+pi = np.pi
 
 # Variable set-up of the real area/volume for each dimension in use
 area_2D = pi*R1[0]**2
@@ -103,23 +113,38 @@ area_5D = (8/15)*(pi**2)*R1[0]**5
 
 if rank==0:
     print("")
-    print(f"Expected area for a 2D ircle via MC, r = {R1[0]}: {I_c}")
-    print(f"Calculated area for a 2D circle: {area_2D}")
+    print(f"Calculated area for a 2D circle via MC, r = {R1[0]}: {I_c}")
+    print(f"Expected area for a 2D circle: {area_2D}")
     print("")
-    print(f"Expected area for 3D Sphere radius via MC, r = {R1[0]}: {I_s}")
-    print(f"Calculated volume for a 3D sphere: {area_3D}")
+    print(f"Calculated area for 3D sphere radius via MC, r = {R1[0]}: {I_s}")
+    print(f"Expected volume for a 3D sphere: {area_3D}")
     print("")
-    print(f"Expected area for 4D Hypersphere radius via MC, r = {R1[0]}: {I_hs_4}")
-    print(f"Calculated volume of a 4D hypersphere: {area_4D}")
+    print(f"Calculated area for 4D hypersphere radius via MC, r = {R1[0]}: {I_hs_4}")
+    print(f"Expected volume of a 4D hypersphere: {area_4D}")
     print("")
-    print(f"Expected area for 5D Hpersphere radius via MC, r = {R1[0]}: {I_hs_5}")
-    print(f"Calculated volume of a 5D hypersphere:{area_5D}") 
+    print(f"Calculated area for 5D hypersphere radius via MC, r = {R1[0]}: {I_hs_5}")
+    print(f"Expected volume of a 5D hypersphere:{area_5D}")
     print("")
 
+# Final calculation timing and the time taken to run it
 t1_b = t.time()
 dt1 = t1_b - t1_a
 print(f"Time taken for calculation on the area of a circle in each dimension = {dt1} secs")
 
+# Set-up to find the speed-up (S), taken from the time taken (T) on each N core(s) to be plotted
+T1 = np.array([28.4527, 28.8680, 28.4984, 28.4362, 26.2481])
+N_c = np.array([1, 2, 4, 8, 16])
+
+S1 = np.zeros(5)
+for i in range(0,4):
+    S1[i+1] =+ T1[0]/T1[i]
+
+plt.figure(1)
+plt.plot(N_c, S1)
+plt.title("Speed up of N cores v N cores")
+plt.ylabel("Speed up for N cores")
+plt.xlabel("N cores")
+plt.savefig("Graph1.jpg")
 
 ###--------------------### Task 2 ###----------------------###
 
@@ -136,36 +161,52 @@ def gauss_function(x, sigma, x_0):
     return (1/(sigma * np.sqrt(2 * pi))) * e((-(x - x_0)**2)/(2 * sigma**2))
 
 # R sets the array used for start or end at [1, 1, 1, 1, 1]
-R = np.repeat(1, 5) 
+R = np.repeat(1, 5)
 
 # Variables used for the standard deviation, and the mean
 vars_1 = np.array([0.4, 3])
 vars_2 = np.array([0.2, 1])
 vars_3 = np.array([0.5, 4])
 
+# Initial measurement for calculation timing
 t2_a = t.time()
 
-gauss_f_1 = Monte_Carlo(-R, R, N, gauss_function, *vars_1)
-g_1 = Monte_Carlo.infinity(gauss_f_1)
+gauss_f_1 = MonteCarlo(-R, R, N, gauss_function, *vars_1)
+g_1 = MonteCarlo.infinity(gauss_f_1)
 
-gauss_f_2 = Monte_Carlo(-R, R, N, gauss_function, *vars_2)
-g_2 = Monte_Carlo.infinity(gauss_f_2)
+gauss_f_2 = MonteCarlo(-R, R, N, gauss_function, *vars_2)
+g_2 = MonteCarlo.infinity(gauss_f_2)
 
-gauss_f_3 = Monte_Carlo(-R, R, N, gauss_function, *vars_3)
-g_3 = Monte_Carlo.infinity(gauss_f_3)
-
+gauss_f_3 = MonteCarlo(-R, R, N, gauss_function, *vars_3)
+g_3 = MonteCarlo.infinity(gauss_f_3)
 
 if rank==0:
     print("")
-    print(f"Expected Gaussian distribution with sigma = {vars_1[0]}, mean = {vars_1[1]}: Integral = {g_1[0]}, Variance = {g_1[1]}, Error = {g_1[2]}")
+    print(f"Gaussian dist. σ = {vars_1[0]}, μ = {vars_1[1]}: Integral = {g_1[0]}, Variance = {g_1[1]}, Error = {g_1[2]}")
     print("")
-    print(f"Expected Gaussian distribution with sigma = {vars_2[0]}, mean = {vars_2[1]}: Integral = {g_2[0]}, Variance = {g_2[1]}, Error = {g_2[2]}")
+    print(f"Gaussian dist. σ = {vars_2[0]}, μ = {vars_2[1]}: Integral = {g_2[0]}, Variance = {g_2[1]}, Error = {g_2[2]}")
     print("")
-    print(f"Expected Gaussian distribution with sigma = {vars_3[0]}, mean = {vars_3[1]}: Integral = {g_3[0]}, Variance = {g_3[1]}, Error = {g_3[2]}")
+    print(f"Gaussian dist. σ = {vars_3[0]}, μ = {vars_3[1]}: Integral = {g_3[0]}, Variance = {g_3[1]}, Error = {g_3[2]}")
 
+# Final calculation timing and the time taken to run it
 t2_b = t.time()
 dt2 = t2_b - t2_a
 print(f"Time taken for Gaussian distribution calculations = {dt2} secs")
+
+# Set-up to find the speed-up (S), taken from the time taken (T) on each N core(s) to be plotted
+T2 = np.array([14.3461, 14.8378, 14.8083, 14.9860, 14.2158])
+N_c = np.array([1, 2, 4, 8, 16])
+
+S2 = np.zeros(5)
+for i in range(0,4):
+    S2[i+1] =+ T2[0]/T2[i]
+
+plt.figure(2)
+plt.plot(N_c, S2)
+plt.title("Speed up of N cores v N cores")
+plt.ylabel("Speed up for N cores")
+plt.xlabel("N cores")
+plt.savefig("Graph2.jpg")
 
 # Finalise the MPI function
 MPI.Finalize()
